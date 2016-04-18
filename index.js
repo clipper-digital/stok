f8e5bc7d0e7ec964568079b58976cbd32d522577
Scott González <scott.gonzalez@gmail.com>
2016-04-18 14:36:20 -0400

    Add logging to the shutdown process
    Require name for registered modules.

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ class Stok {
     this.server = this.serverProxy.getServer();
 
     this.registerModule({
+      name: 'Hapi Server',
       shutdown: () => {
         if (!this.serverProxy.isStarted()) {
           return Promise.resolve();
@@ -43,24 +44,42 @@ class Stok {
   }
 
   registerModule(module) {
+    if (!module.hasOwnProperty('name')) {
+      throw new Error('Missing required field: name');
+    }
+
+    this.server.log(['module'], `Module registered: ${module.name}`);
     this.registeredModules.push(module);
   }
 
   shutdown() {
-    return shutdownModules(this.registeredModules);
+    return this._shutdownModules(this.registeredModules);
+  }
+
+  _shutdownModules(modules) {
+    const module = modules.pop();
+
+    if (!module) {
+      this.server.log(['shutdown'], 'Shutdown complete');
+      return Promise.resolve();
+    }
+
+    this.server.log(['shutdown', 'module'], `Module shutdown starting: ${module.name}`);
+
+    return module.shutdown()
+      .then(() => {
+        this.server.log(['shutdown', 'module'], `Module shutdown complete: ${module.name}`);
+        return this._shutdownModules(modules);
+      })
+      .catch((error) => {
+        this.server.log(['shutdown', 'module', 'error'], `Module shutdown error: ${module.name}`);
+        this.server.log(['shutdown', 'module', 'error'], error.stack);
+
+        throw error;
+      });
   }
 }
 
 Stok.loadConfiguration = loadConfiguration;
-
-function shutdownModules(modules) {
-  let module = modules.pop();
-
-  if (!module) {
-    return Promise.resolve();
-  }
-
-  return module.shutdown().then(() => shutdownModules(modules));
-}
 
 module.exports = Stok;
